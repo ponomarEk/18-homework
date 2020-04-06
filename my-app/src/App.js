e57e509727e00a63ad90eac3e90321a385d97f16
Sasha <ponomarek91@gmail.com>
2020-04-06 17:42:13 +0300

    done

---
+++ my-app/src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+
+
+import Layout from './layout/index';
+import Home from './Pages/Home/home';
+import Posts from './Pages/Posts/posts';
+import Photos from './Pages/Photos/photos';
+import Contacts from './Pages/Contacts/contactBook';
+import Render from './Pages/ContactsRender/render'
+
+
+export default function App() {
+  return (
+    <Router>
+      <Layout>
+        <Switch>
+        <Route path="/contacts/:id" >
+            <Render/>
+          </Route>
+          <Route path="/contacts">
+            <Contacts/>
+          </Route>
+          <Route path="/photos">
+            <Photos />
+          </Route>
+          <Route path="/posts">
+            <Posts />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Layout>
+    </Router>
+  );
+}
