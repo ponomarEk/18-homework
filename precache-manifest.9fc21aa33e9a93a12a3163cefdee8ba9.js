60419b02084606e96057f180d8735847b69e1dbc
Sasha <ponomarek91@gmail.com>
2020-04-06 17:43:30 +0300

    Updates

---
+++ precache-manifest.9fc21aa33e9a93a12a3163cefdee8ba9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f5d33f56a3040c14a6ac4e5ee22a0b",
+    "url": "/18-Homework/index.html"
+  },
+  {
+    "revision": "80c9d4707dfecb9525ee",
+    "url": "/18-Homework/static/css/main.2352a681.chunk.css"
+  },
+  {
+    "revision": "c622858ca6c79286824f",
+    "url": "/18-Homework/static/js/2.72bb5baf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/18-Homework/static/js/2.72bb5baf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80c9d4707dfecb9525ee",
+    "url": "/18-Homework/static/js/main.41917e38.chunk.js"
+  },
+  {
+    "revision": "5e9669f191e696aaf1b4",
+    "url": "/18-Homework/static/js/runtime-main.74688f11.js"
+  },
+  {
+    "revision": "72c941bdd563640381785f90b114a0f5",
+    "url": "/18-Homework/static/media/01e81a35e0d119aed42d2fb15ae6d7e5.72c941bd.jpg"
+  },
+  {
+    "revision": "ef3a41855946f9e8f9411d21a990ecb2",
+    "url": "/18-Homework/static/media/32_1920_1200_beautypic.ru.ef3a4185.jpg"
+  },
+  {
+    "revision": "783bc1bdaadb65c0e8d4d45204f8b42c",
+    "url": "/18-Homework/static/media/prikolnye-kartinki-pro-leto-na-rabochij-stol-1.783bc1bd.jpg"
+  },
+  {
+    "revision": "88c0f6eeb25f75c7adddb9dee335b0a4",
+    "url": "/18-Homework/static/media/unnamed.88c0f6ee.jpg"
+  }
+]);
